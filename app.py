565b447fd515bc0bbadd10ee69a239d8dd45b34c
ytgTonz <ytgtonz@gmail.com>
2025-05-30 10:18:17 +0200

    add database connection and save forms

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import Length
+from pymongo import MongoClient
+import os, request
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -8,7 +17,39 @@ app = Flask(__name__)
 def index():
     return render_template('landing-freelancer.html', title='YoTag')
 
+class MessageForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+
+
+def edit_profile():
+    form = MessageForm()
+    if form.validate_on_submit():
+        form.username.data =  request.form['name']  # Retrieve 'name' input
+        form.email.data =  request.form['email']  # Retrieve 'email' input
+        form.message.data  = request.form['message'] # Retrieve 'message' input
+        
+        message_data = {
+            "username": form.username.data,
+            "email": form.email.data,
+            "message": form.message.data
+        }
+        collection.insert_one(message_data)
+        print("Report saved to MongoDB Atlas")
+    return render_template('landing-freelancer.html', title='YoTag', form=form)
+    
+
+
+# MongoDB Atlas connection
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["clientlist"]
+collection = db["contacts"]
+
+    
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
-    
